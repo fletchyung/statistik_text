eef8de999228ea54e789f85292703014aa40f452
Aaaa <Sss@s>
2022-12-19 16:20:54 +0300

    ghhh2s

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.io .*;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Main {
+
+    public static void main(String[] args)  throws FileNotFoundException {
+        getWordCount ("C:\\Users\\Артём\\IdeaProjects\\statistika\\src\\com\\company\\vvedite_text.txt");
+    }
+    public static void getWordCount(String filename) throws FileNotFoundException {
+        File file = new File(filename);
+        Scanner scanner = new Scanner (file);
+
+
+        String wording = "";
+        while (scanner.hasNextLine()) {
+            wording = scanner.nextLine();
+        }
+        System.out.println("Всего слов: " + wording.split(" ").length);
+        System.out.println("Всего символов: " + wording.length());
+        System.out.println("Всего символов без пробела: " + wording.replaceAll(" ", "").length());
+    }
+}
+
+
+
